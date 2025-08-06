58a352b6979626af80debbfe2c9866ab315e2164
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-08-06 13:40:28 -0500

    Created Student.jsx. Initialized components

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// props = read-only properties that are shared between components.
+//          A parent component can send data to a child component.
+//          <Component key=value /> 
+
+import Student from './Student.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+    <Student name="Spongebob"/>
       </>
-  )
+  );
 }
 
 export default App
